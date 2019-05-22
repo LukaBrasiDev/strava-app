798eecc3fde2e290cfe06f67f897b71ee6f70c99
Luca Brasi <lukasz.anaczkowski@gmail.com>
2019-05-22 17:31:56 +0200

    initial

---
+++ src/main/java/pl/lukabrasi/stravaapp/StravaAppApplication.java
@@ -0,0 +1,13 @@
+package pl.lukabrasi.stravaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StravaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StravaAppApplication.class, args);
+    }
+
+}
