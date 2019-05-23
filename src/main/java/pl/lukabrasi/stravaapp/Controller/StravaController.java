a038c3daf9468b35ff201dc322b6198c96d4cb13
lanczako <lukasz.anaczkowski@thalesgroup.com>
2019-05-23 13:39:19 +0200

    - service i controller

--- src/main/java/pl/lukabrasi/stravaapp/Controller/StravaController.java
+++ src/main/java/pl/lukabrasi/stravaapp/Controller/StravaController.java
@@ -1,7 +1,32 @@
 package pl.lukabrasi.stravaapp.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import pl.lukabrasi.stravaapp.service.StravaService;
 
 @Controller
 public class StravaController {
+
+    final StravaService stravaService;
+
+    @Autowired
+    public StravaController(StravaService stravaService) {
+        this.stravaService = stravaService;
+    }
+
+    @GetMapping("/")
+    public String getStats(Model model)  {
+        String authenticate = stravaService.getAuthenticate();
+        model.addAttribute("auth", stravaService.getAuthenticate());
+
+        return "index";
+    }
+
+
+
+
+
+
 }
--- src/main/java/pl/lukabrasi/stravaapp/service/StravaService.java
+++ src/main/java/pl/lukabrasi/stravaapp/service/StravaService.java
@@ -1,7 +1,39 @@
 package pl.lukabrasi.stravaapp.service;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class StravaService {
+
+    @Value("${api.clientsecret.key}")
+    public String clientSecret;
+
+    @Value("${api.clientid.key}")
+    public String clientId;
+
+    public String redirectUri = "http://localhost:8080/";
+
+
+    public String getAuthenticate() {
+        RestTemplate restTemplate = getRestTemplate();
+        String url = "https://www.strava.com/oauth/authorize/?client_id=" + clientId + "&redirect_uri=" + redirectUri + "&redirect_uri=" + redirectUri + "&response_type=code&approval_prompt=auto&scope=activity:read";
+
+        String accountDto = restTemplate.getForObject(url, String.class);
+        System.out.println(accountDto);
+        return accountDto;
+    }
+
+
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+
+        return new RestTemplate();
+    }
+
+
+
 }
