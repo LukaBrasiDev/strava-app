5305c6e0547ac4030e154eee5d5dd10fba9634ce
Luca Brasi <lukasz.anaczkowski@gmail.com>
2019-07-16 11:22:28 +0200

    - index page with redirect link - redirect for authorization ok initial

--- src/main/java/pl/lukabrasi/stravaapp/service/StravaService.java
+++ src/main/java/pl/lukabrasi/stravaapp/service/StravaService.java
@@ -14,12 +14,19 @@ public class StravaService {
     @Value("${api.clientid.key}")
     public String clientId;
 
-    public String redirectUri = "http://localhost:8080/";
+    public String redirectUri = "http://strava.com";
 
 
     public String getAuthenticate() {
         RestTemplate restTemplate = getRestTemplate();
-        String url = "https://www.strava.com/oauth/authorize/?client_id=" + clientId + "&redirect_uri=" + redirectUri + "&response_type=code&approval_prompt=auto&scope=activity:read";
+        String url = "https://www.strava.com/oauth/authorize/?client_id=" + clientId + "&response_type=code&redirect_uri=" + redirectUri + "&approval_prompt=force";
+        String accountDto = restTemplate.getForObject(url, String.class);
+        return accountDto;
+    }
+
+    public String getAuthenticate2() {
+        RestTemplate restTemplate = getRestTemplate();
+        String url = "https://www.strava.com/oauth/authorize/?client_id=" + clientId + "&redirect_uri=" + redirectUri + "&response_type=code&scope=read";
         String accountDto = restTemplate.getForObject(url, String.class);
         return accountDto;
     }
